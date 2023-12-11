6ad592cc2a7d8d8630f873b7348d4bd390d00457
Lucas <Lucas@DANI-PC>
2023-12-11 14:37:27 -0300

    Add project files.

---
+++ Passwords-Gen.API/Controllers/PasswordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Passwords_Gen.Domain.Interfaces;
+using Passwords_Gen.Service.Services;
+
+namespace Passwords_Gen.Controllers
+{
+    [ApiController]
+    [Route("api/password-gen")]
+    public class PasswordsController : ControllerBase
+    {
+        private readonly IPasswordService _passwordService;
+        public PasswordsController(IPasswordService passwordService)
+        {
+            _passwordService = passwordService;
+        }
+
+        [HttpGet]
+        public IActionResult GetPassword(int length)
+        {
+            var result = _passwordService.GenerateRandomPassword(length);
+            return Ok(result);
+        }
+    }
+}
---
+++ Passwords-Gen.Service/Services/PasswordService.cs
@@ -0,0 +1,22 @@
+﻿using Passwords_Gen.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passwords_Gen.Service.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public string GenerateRandomPassword(int length)
+        {
+            var charsLetters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&*()[{}]/?";
+            Random rndLetters = new();
+            var randLetters = new String(Enumerable.Repeat(charsLetters, length)
+                .Select(s => s[rndLetters.Next(s.Length)]).ToArray());
+            var result = new StringBuilder(randLetters);
+            return result.ToString();
+        }
+    }
+}
